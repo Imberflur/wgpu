6cb6852549ce71c0561bd6130dc428ad41ad19e5
Imbris <imbrisf@gmail.com>
2023-07-28 10:52:44 -0400

    Track peak frame time

--- examples/common/src/framework.rs
+++ examples/common/src/framework.rs
@@ -283,7 +283,7 @@ fn start<E: Example>(
     #[cfg(not(target_arch = "wasm32"))]
     let mut last_frame_inst = Instant::now();
     #[cfg(not(target_arch = "wasm32"))]
-    let (mut frame_count, mut accum_time) = (0, 0.0);
+    let (mut frame_count, mut accum_time, mut peak_time) = (0, 0.0, 0.0);
 
     log::info!("Entering render loop...");
     event_loop.run(move |event, _, control_flow| {
@@ -358,15 +358,20 @@ fn start<E: Example>(
             event::Event::RedrawRequested(_) => {
                 #[cfg(not(target_arch = "wasm32"))]
                 {
-                    accum_time += last_frame_inst.elapsed().as_secs_f32();
+                    let s = last_frame_inst.elapsed().as_secs_f32();
                     last_frame_inst = Instant::now();
+                    accum_time += s;
+                    peak_time = f32::max(peak_time, s);
                     frame_count += 1;
                     if frame_count == 100 {
                         println!(
-                            "Avg frame time {}ms",
-                            accum_time * 1000.0 / frame_count as f32
+                            "Avg frame time  {}ms\n\
+                             Peak frame time {}ms",
+                            accum_time * 1000.0 / frame_count as f32,
+                            peak_time * 1000.0,
                         );
                         accum_time = 0.0;
+                        peak_time = 0.0;
                         frame_count = 0;
                     }
                 }
